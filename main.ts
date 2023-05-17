aad499292745b2437c37389d76b9088d06e8a4b5
hkhs40508 <131329413+hkhs40508@users.noreply.github.com>
2023-05-18 07:51:45 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index <= 9; index++) {
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        basic.showNumber(1 + index)
+    }
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.showString("stop!")
+})
